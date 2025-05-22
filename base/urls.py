a03e716859bc5a0c13541c3a10237adf45f609a8
Marco Pienaar <markopienaar2@gmail.com>
2025-05-22 12:11:24 +0200

    added a "delete" room method to remove created rooms

--- base/urls.py
+++ base/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('room/<str:pk>/', views.room, name='room'),
     
     path('update-room/<str:pk>', views.updateRoom, name='update-room'),
-    path('create-room/', views.createRoom, name='create-room')
+    path('create-room/', views.createRoom, name='create-room'),
+    path('delete-room/<str:pk>', views.deleteRoom, name='delete-room')
 ]
