cc9d10b7ca8f3b97c513137f06931801a7230fbc
Marco Pienaar <markopienaar2@gmail.com>
2025-05-22 13:30:03 +0200

    Imported 'Q' so that we can search via name and description

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -11,7 +12,11 @@ from .forms import RoomForm
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
-    rooms = Room.objects.filter(topic__name__icontains=q)
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name_icontains=q) |
+        Q(description_icontains=q)
+        )
 
     topics = Topic.objects.all()
 
