ce825d03fa5764911e00feb4ce6c4121c2ed125f
Marco Pienaar <markopienaar2@gmail.com>
2025-05-23 11:59:49 +0200

    added a redirect back too home if user is logged in succesfully

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db.models import Q
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -22,7 +23,11 @@ def loginPage(request):
         except:
             messages.error(request, 'User does not exist')
 
+        user = authenticate(request, username=username, password=password)
 
+        if user is not None:
+            login(request, user)
+            return redirect('home')
 
     context = {}
     return render(request, 'base/login_register.html', context)
